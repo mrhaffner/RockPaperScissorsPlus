f5169c5277e51ba65ab6fb64b00e5a5f02a43110
mrhaffner <mrhaffner13@gmail.com>
2021-05-02 17:49:29 -0500

    Initial Commit

---
+++ RockPaperScissorPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorPlus
+//
+//  Created by Matt Haffner on 5/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissorPlus/RockPaperScissorPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorPlusApp.swift
+//  RockPaperScissorPlus
+//
+//  Created by Matt Haffner on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
